6abc7b980ceeafa109dbf52cfa551fb7d8a36238
lichengmeng123 <2892862386@qq.com>
2025-10-20 19:19:09 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,15 @@
-//
-//
-//
+//202511716116
+//2892862386@qq.com
+//李澄萌
 #include <stdio.h>
 int main()
 {
-    printf("Hello World");
-    return 0;
+   float celsius;
+   printf("请输入摄氏温度：");
+   scanf("%f",&celsius);
+
+   float fahrenheit=celsius*9.0/5.0+32;
+   printf("华氏温度为：%.1f\n",fahrenheit);
+
+   return 0;
 }
