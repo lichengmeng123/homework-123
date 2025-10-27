9a4b08c85c11dee424189fdf93877f238aa0804e
lichengmeng123 <2892862386@qq.com>
2025-10-27 19:45:09 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,14 +2,28 @@
 //2892862386@qq.com
 //李澄萌
 #include <stdio.h>
-int main()
-{
-   float celsius;
-   printf("请输入摄氏温度：");
-   scanf("%f",&celsius);
-
-   float fahrenheit=celsius*9.0/5.0+32;
-   printf("华氏温度为：%.1f\n",fahrenheit);
-
-   return 0;
-}
+ int main()
+ {
+     int n, i, is_prime = 1; 
+     printf("请输入小于50的正整数密钥：");
+     scanf("%d", &n);
+     for (i = 2; i < n; i++) 
+     {
+         if (n % i == 0) 
+         {
+             is_prime = 0;
+             break; 
+         }
+     }
+     if (n == 1)
+      {
+         printf("密钥不安全，请重新输入\n");
+     } else if (is_prime == 1) 
+     {
+         printf("密钥安全，密码设置成功\n");
+     } else
+      {
+         printf("密钥不安全，请重新输入\n");
+     }
+     return 0;
+ }
