ef26eeef239a9cac02d1112100c7cee00ce5ae6b
lichengmeng123 <2892862386@qq.com>
2025-10-27 20:13:35 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -2,24 +2,26 @@
 //2892862386@qq.com
 //李澄萌
 #include <stdio.h>
-int main(){
-    int score;
-    printf("请输入学生的整数成绩（0-100）");
-    scanf("%d",&score);  
-
-    if(score>=80 && score<=100)
+ int main()
+ {
+    int num,digit1,digit2,digit3;
+    int count = 0;
+    for(num=100;num<=999;num++)
     {
-        printf("A");
-    }else if (score>=80 && score <=89){
-        printf("B");
-    }else if (score>=70 && score <=79){
-        printf("C");
-    }else if (score>=60 && score <=69){
-        printf("D");
-    }else if (score>=0 && score<60){
-        printf("E");
+        digit1 = num / 100;
+        digit2 = (num / 10)%10;
+        digit3 = num % 10;
+        if(digit1*digit1*digit1+digit2*digit2*digit2+digit3*digit3*digit3 == num)
+        {
+            if(count > 0)
+            {
+            printf(" ");
+            }
+            printf("%d",num); 
+            count++;
+        }
     }
-
+    printf("\n");
     return 0;
+ }
 
-}
