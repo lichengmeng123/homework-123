e3213f4e92df607b97ed980685a69660512c7a94
lichengmeng123 <2892862386@qq.com>
2025-11-03 20:28:06 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,28 +2,25 @@
 //2892862386@qq.com
 //李澄萌
 #include <stdio.h>
- int main()
+int main() 
 {
-     int n, i, is_prime = 1; 
-     printf("请输入小于50的正整数密钥：");
-     scanf("%d", &n);
-     for (i = 2; i < n; i++) 
-     {
-         if (n % i == 0) 
+    int matrix[3][3];
+    for (int i = 0; i < 3; i++) 
+    {
+        for (int j = 0; j < 3; j++)
          {
-             is_prime = 0;
-             break; 
-         }
-     }
-     if (n == 1)
-      {
-         printf("密钥不安全，请重新输入\n");
-     } else if (is_prime == 1) 
-     {
-         printf("密钥安全，密码设置成功\n");
-     } else
-      {
-         printf("密钥不安全，请重新输入\n");
-     }
-     return 0;
- }
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    for (int i = 0; i < 3; i++) 
+    {
+        for (int j = 0; j < 3; j++)
+         {
+    
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
