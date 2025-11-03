42ac06e527f6acf987c8d82ca7d4a991de635806
lichengmeng123 <2892862386@qq.com>
2025-11-03 20:56:52 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -1,28 +1,23 @@
 //202511716116
 //2892862386@qq.com
 //李澄萌
-#include <stdio.h>
-int main() {
-    int matrix[3][3];
-    int transposed[3][3];
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            scanf("%d", &matrix[i][j]);
-        }
+#include<stdio.h>
+int main()
+{int arr[3][3];
+    int i,j;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {scanf("%d",&arr[i][j]);}
     }
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            transposed[j][i] = matrix[i][j];
-        }
+     for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        if(j==0)
+        {printf("%d",arr[j][i]);}
+        else  {printf(" %d",arr[j][i]);}
+         printf("\n");
     }
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d ", transposed[i][j]);
-        }
-        printf("\n");
-    }
-
+   
     return 0;
-}
+    }
