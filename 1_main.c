70a558082543dc74c568d44506fc35451a321638
lichengmeng123 <2892862386@qq.com>
2025-11-03 20:38:13 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,21 +2,17 @@
 //2892862386@qq.com
 //李澄萌
 #include <stdio.h>
-int main() 
-{
+int main() {
     int matrix[3][3];
-    for (int i = 0; i < 3; i++) 
-    {
-        for (int j = 0; j < 3; j++)
-         {
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             scanf("%d", &matrix[i][j]);
         }
     }
-    for (int i = 0; i < 3; i++) 
-    {
-        for (int j = 0; j < 3; j++)
-         {
-    
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
@@ -24,3 +20,4 @@ int main()
 
     return 0;
 }
+
