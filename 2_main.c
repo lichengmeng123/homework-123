dbbcf0b0350269f6bc072d590e9e23fc1f2732d7
lichengmeng123 <2892862386@qq.com>
2025-11-18 21:31:24 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -1,3 +1,20 @@
 //202511716116
 //2892862386@qq.com
 //李澄萌
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int *p = arr; 
+    for (int i = 0; i < 5; i++) {
+        *(p + i) *= 2;
+    }
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", *(p + i));
+    }
+    printf("\n");
+    return 0;
+}
