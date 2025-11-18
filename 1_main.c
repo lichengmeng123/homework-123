cbd35caa74b370fae443373e069e19f482fe317d
lichengmeng123 <2892862386@qq.com>
2025-11-18 21:28:39 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,3 +1,13 @@
 //202511716116
 //2892862386@qq.com
 //李澄萌
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    int *p = &a;
+    *p += 10; 
+    printf("%d,%d\n", a, *p);
+    return 0;
+}
