fac179ca8674f45d093a42954cc3ae20a3e64e95
lichengmeng123 <2892862386@qq.com>
2025-11-18 21:33:14 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -1,39 +1,4 @@
 //202511716116
 //2892862386@qq.com
 //李澄萌
-#include <stdio.h>
 
-void bubbleSort(int arr[], int n) 
-{
-    for (int i = 0; i < n - 1; i++) 
-    {
-        for (int j = 0; j < n - 1 - i; j++) 
-        {
-            if (arr[j] > arr[j + 1]) 
-            {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-}
-
-int main()
- {
-    int numbers[10];
-
-    for (int i = 0; i < 10; i++) 
-    {
-        scanf("%d", &numbers[i]);
-    }
-    bubbleSort(numbers, 10);
-
-    for (int i = 0; i < 10; i++) 
-    {
-        printf("%d ", numbers[i]);
-    }
-    printf("\n");
-
-    return 0;
-}
