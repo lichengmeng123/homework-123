a8b1944d5e1c15c26028874de54ceb2f16737aae
lichengmeng123 <2892862386@qq.com>
2025-11-18 21:28:27 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,24 +1,3 @@
 //202511716116
 //2892862386@qq.com
 //李澄萌
-#include<stdio.h>
-int main()
-{
-    int arr[3][3];
-    int i,j;
-    for(i=0;i<3;i++)
-    {
-        for(j=0;j<3;j++)
-        {scanf("%d",&arr[i][j]);}
-    }
-     for(i=0;i<3;i++)
-    {
-        for(j=0;j<3;j++)
-        if(j==0)
-        {printf("%d",arr[i][j]);}
-        else  {printf(" %d",arr[i][j]);}
-         printf("\n");
-    }
-   
-    return 0;
-    }
